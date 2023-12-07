98b24e9736cee2928c02e1eccab584f1ad380dd6
Klittar <100770588+Klittar@users.noreply.github.com>
2023-12-07 21:40:19 +0300

    Add files via upload

---
+++ sender.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+$_SESSION['formSubmitted'] = true;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $text = $_POST['text'];
+
+    $to = "kakashkavot@gmail.com";
+    $date = date("d.m.Y");
+    $time = date("h:i");
+    $from = $email;
+    $subject = "Заявка c сайта";
+
+    $msg = "
+    Имя: $name /n
+    Телефон: $phone /n
+    Почта: $email /n
+    Текст: $text";
+    mail($to, $subject, $msg, "From: $from ");
+}
+?>
